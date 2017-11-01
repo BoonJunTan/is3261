926df23c964242db259265df8db4d1a29e27acb9
Dyllan <Tan@Boons-MacBook-Pro.local>
2017-11-01 21:19:12 +0800

    Done with Bottom Navigation Bar

--- app/src/main/java/com/tanboonjun/oneandroid/MainActivity.java
+++ app/src/main/java/com/tanboonjun/oneandroid/MainActivity.java
@@ -1,13 +1,99 @@
 package com.tanboonjun.oneandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mBottomNav = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        mBottomNav.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        selectFragment(item);
+                        return true;
+                    }
+                });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = mBottomNav.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                frag = new HomeFragment();
+                break;
+            case R.id.action_task:
+                frag = new TaskFragment();
+                break;
+            case R.id.action_challenge:
+                frag = new LeaderboardFragment();
+                break;
+            case R.id.action_profile:
+                frag = new ProfileFragment();
+                break;
+        }
+
+        // Update selected item
+        mSelectedItem = item.getItemId();
+
+        // Uncheck the other items.
+        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        // Not sure why this is needed for a very weird bug when app launched
+        mBottomNav.getMenu().getItem(0).setChecked(true);
+
+        if (frag != null) {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.frame_layout, frag);
+            ft.commit();
+        }
+    }
+
 }
