4119a7232851885169782500702024437258d2e5
Dyllan <>
2017-11-07 13:55:35 +0800

    Added task view UI but not done yet

--- app/src/main/java/com/tanboonjun/oneandroid/TaskFragment.java
+++ app/src/main/java/com/tanboonjun/oneandroid/TaskFragment.java
@@ -1,11 +1,13 @@
 package com.tanboonjun.oneandroid;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 /**
@@ -18,12 +20,20 @@ public class TaskFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        TableLayout task_list_table = (TableLayout) view.findViewById(R.id.task_table_layout);
+
+        for (int i = 0; i < 5; i++) {
+            View taskRowView = inflater.inflate(R.layout.task_table_row, container, false);
+            TableRow task_list_row = (TableRow) taskRowView.findViewById(R.id.task_table_row);
+
+            task_list_table.addView(task_list_row);
+        }
+
+        return view;
     }
 
 }
